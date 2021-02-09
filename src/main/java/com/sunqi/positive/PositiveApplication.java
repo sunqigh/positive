cab770fda6b54b250f9f486d897f16960133d75a
Sun Qi <sunqi@csiisz.com>
2021-02-09 11:31:24 +0800

    feat: init project

---
+++ src/main/java/com/sunqi/positive/PositiveApplication.java
@@ -0,0 +1,13 @@
+package com.sunqi.positive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PositiveApplication.class, args);
+    }
+
+}
